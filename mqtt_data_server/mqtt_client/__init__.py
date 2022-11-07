cd91ad64627f1494a82833d8043e0ec33b2efbfa
Lee Jihoon <jihoolee@student.42seoul.kr>
2022-11-07 20:53:04 +0900

    [ADD]: mqtt module added

--- mqtt_data_server/mqtt_client/__init__.py
+++ mqtt_data_server/mqtt_client/__init__.py
@@ -1,17 +1,3 @@
-from threading import Thread
-from time import sleep
+from . import mqtt
 
-class ThreadTest:
-    def __init__(self):
-        self.num = 0
-
-    def print_num(self):
-        print("subthread start")
-        while True:
-            print(self.num)
-            self.num += 1
-            sleep(1)
-
-print('main thread start')
-th = Thread(target=ThreadTest().print_num, daemon=True)
-th.start()
+# mqtt.client.loop_start()
--- mqtt_data_server/mqtt_client/models.py
+++ mqtt_data_server/mqtt_client/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    temperature = models.FloatField()
+    humidity = models.FloatField()
